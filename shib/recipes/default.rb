b9fbab4f736b4f6e8c5edd1a2b6f21e9f47200d6
Ali Mohamed <ali.ahmmed.mohamed@gmail.com>
2014-08-21 16:00:14 +0200

    update

---
+++ shib/recipes/default.rb
@@ -0,0 +1,16 @@
+node[:deploy].each do |app_name, deploy|
+
+  script "install_nothing" do
+    interpreter "bash"
+    user "root"
+    cwd "#{deploy[:deploy_to]}/current"
+    code <<-EOH
+    apt-get install curl
+	curl -k -O http://pkg.switch.ch/switchaai/SWITCHaai-swdistrib.asc
+	apt-key add SWITCHaai-swdistrib.asc
+	echo 'deb http://pkg.switch.ch/switchaai/ubuntu precise main' | sudo tee /etc/apt/sources.list.d/SWITCHaai-swdistrib.list > /dev/null
+	apt-get update
+	apt-get install shibboleth
+    EOH
+  end
+end
