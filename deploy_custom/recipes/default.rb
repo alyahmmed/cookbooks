1a62a88366f6b5b98de8f229c6d80c33bbfff0d1
Ali Mohamed <ali.ahmmed.mohamed@gmail.com>
2014-08-27 21:35:34 +0200

    Update default.rb

--- deploy_custom/recipes/default.rb
+++ deploy_custom/recipes/default.rb
@@ -6,10 +6,10 @@ node[:deploy].each do |app_name, deploy|
     interpreter "bash"
     user "root"
     code <<-EOH
-    sed -i "9i Require shibboleth" /etc/apache2/sites-available/#{app_name}.conf
-    sed -i "9i ShibUseEnvironment On" /etc/apache2/sites-available/#{app_name}.conf
-    sed -i "9i AuthType shibboleth" /etc/apache2/sites-available/#{app_name}.conf
     sed -i "9i ShibRequireSession Off" /etc/apache2/sites-available/#{app_name}.conf
+    sed -i "9i SetHandler shib" /etc/apache2/sites-available/#{app_name}.conf
+    sed -i "9i AuthType shibboleth" /etc/apache2/sites-available/#{app_name}.conf
+    sed -i "9i Require shibboleth" /etc/apache2/sites-available/#{app_name}.conf
     service apache2 restart
     EOH
   end
