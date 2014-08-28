5029973d4a9b020b154d7cd41f2a8fee427b3686
Ali Mohamed <ali.ahmmed.mohamed@gmail.com>
2014-08-28 02:37:55 +0200

    update

--- shib/recipes/deploy.rb
+++ shib/recipes/deploy.rb
@@ -3,14 +3,17 @@ node[:deploy].each do |app_name, deploy|
 	template "shibboleth2" do
 		source "shibboleth2.xml.erb"
 		path "/etc/shibboleth/shibboleth2.xml"
+		variables(node['shib'])
 	end
 	
 	template "shibboleth2-ssl-key" do
+		mode 0644
 		source 'ssl.key.erb'
 		path "/etc/shibboleth/sp-key.pem"
 	end
 	
 	template "shibboleth2-ssl-cert" do
+		mode 0644
 		source 'ssl.cert.erb'
 		path "/etc/shibboleth/sp-cert.pem"
 	end
