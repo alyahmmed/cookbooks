79c88bf9de4b6f008db0328afde3c1ac87f552b4
Ali Mohamed <ali.ahmmed.mohamed@gmail.com>
2014-08-28 02:23:05 +0200

    update

--- shib/recipes/deploy.rb
+++ shib/recipes/deploy.rb
@@ -1,14 +1,17 @@
 node[:deploy].each do |app_name, deploy|
 
-	template "/etc/shibboleth/shibboleth2.xml" do
+	template "shibboleth2" do
 		source "shibboleth2.xml.erb"
-		owner "root"
-		group "root"
-		variables({
-			:x_men => "are keen"
-		})
-		only_if do
-		  deploy[:ssl_support]
-		end
+		path "/etc/shibboleth/shibboleth2.xml"
+	end
+	
+	template "shibboleth2-ssl-key" do
+		source 'ssl.key.erb'
+		path "/etc/shibboleth/sp-key.pem"
+	end
+	
+	template "shibboleth2-ssl-cert" do
+		source 'ssl.cert.erb'
+		path "/etc/shibboleth/sp-cert.pem"
 	end
 end
