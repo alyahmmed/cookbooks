a3c5f150f5b14793144b788b0d0d3c3d36fe29d6
Ali Mohamed <ali.ahmmed.mohamed@gmail.com>
2014-09-16 11:55:35 +0200

    Update deploy.rb

--- shib/recipes/deploy.rb
+++ shib/recipes/deploy.rb
@@ -1,4 +1,36 @@
 node[:deploy].each do |app_name, deploy|
+	
+	include_recipe 'deploy::default'
+  
+	script "deploy_clean" do
+		interpreter "bash"
+		user "root"
+		line = 9
+		code <<-EOH
+		sed -i "#{line}i ShibUseHeaders On" /etc/apache2/sites-available/#{app_name}.conf
+		sed -i "#{line}i SetHandler shib" /etc/apache2/sites-available/#{app_name}.conf
+		sed -i "#{line}i Require shibboleth" /etc/apache2/sites-available/#{app_name}.conf
+		sed -i "#{line}i ShibRequireSession Off" /etc/apache2/sites-available/#{app_name}.conf
+		sed -i "#{line}i AuthType shibboleth" /etc/apache2/sites-available/#{app_name}.conf
+		service apache2 restart
+		EOH
+	end  
+	
+	script "deploy_clean_https" do
+		if node[:deploy][app_name][:ssl_support]
+			interpreter "bash"
+			user "root"
+			line = 69
+			code <<-EOH
+			sed -i "#{line}i ShibUseHeaders On" /etc/apache2/sites-available/#{app_name}.conf
+			sed -i "#{line}i SetHandler shib" /etc/apache2/sites-available/#{app_name}.conf
+			sed -i "#{line}i Require shibboleth" /etc/apache2/sites-available/#{app_name}.conf
+			sed -i "#{line}i ShibRequireSession Off" /etc/apache2/sites-available/#{app_name}.conf
+			sed -i "#{line}i AuthType shibboleth" /etc/apache2/sites-available/#{app_name}.conf
+			service apache2 restart
+			EOH
+		end
+	end
 
 	template "shibboleth2" do
 		source "shibboleth2.xml.erb"
