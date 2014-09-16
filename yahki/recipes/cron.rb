d1f9ae0f2f77050d7dc66b5eeb7e77ac899fa528
Ali Mohamed <ali.ahmmed.mohamed@gmail.com>
2014-09-16 17:40:52 +0200

    Update cron.rb

--- yahki/recipes/cron.rb
+++ yahki/recipes/cron.rb
@@ -1,7 +1,16 @@
 node[:deploy].each do |app_name, deploy|
+  
+  script "cron_clean" do
+	interpreter "bash"
+	user "root"
+	code <<-EOH
+	mdkir /srv/www/#{app_name}/current/tmp/time
+	chmod 777 /srv/www/#{app_name}/current/tmp/time
+	EOH
+  end 
 
   cron "job_name" do
 	minute "*/5"
-	command "cd /srv/www/#{app_name}/current/tmp && touch $RANDOM"
-end
+	command "cd /srv/www/#{app_name}/current/tmp/time && touch $RANDOM"
+  end
 end
