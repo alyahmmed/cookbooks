5a2e4575d225b390c7ea807a88bd0324ee73c575
Ali Mohamed <ali.ahmmed.mohamed@gmail.com>
2014-09-16 16:40:47 +0200

    Update cron.rb

--- yahki/recipes/cron.rb
+++ yahki/recipes/cron.rb
@@ -2,6 +2,6 @@ node[:deploy].each do |app_name, deploy|
 
   cron "job_name" do
 	minute "*/5"
-	command "cd /srv/www/#{app_name}/current/tmp/time && touch $RANDOM"
+	command "cd /srv/www/#{app_name}/current/tmp && touch $RANDOM"
 end
 end
