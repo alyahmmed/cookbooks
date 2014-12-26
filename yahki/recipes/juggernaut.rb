9abb73ab9518f582db8cb6fdbb3c5491f56344b3
Ali Mohamed <ali.ahmmed.mohamed@gmail.com>
2014-12-26 19:20:12 +0200

    juggernaut

--- yahki/recipes/juggernaut.rb
+++ yahki/recipes/juggernaut.rb
@@ -1,8 +1,8 @@
 node[:deploy].each do |app_name, deploy|
 
   %w{redis-server nodejs npm}.each do |pkg|
-	package pkg do
-	end
+  	package pkg do
+  	end
   end
 
   script "install_juggernaut" do
@@ -11,16 +11,6 @@ node[:deploy].each do |app_name, deploy|
     code <<-EOH
     npm config set registry http://registry.npmjs.org/
     npm install -g juggernaut
-
-    EOH
-  end
-
-  script "run_juggernaut" do
-    interpreter "bash"
-    user "root"
-    code <<-EOH
-    redis-server &
-    juggernaut &
     EOH
   end
 end
