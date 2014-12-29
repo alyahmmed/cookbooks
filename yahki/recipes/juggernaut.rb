5fafd998b7a7868fe6c6a77f7617210771a4ed42
Ali Mohamed <ali.ahmmed.mohamed@gmail.com>
2014-12-29 13:21:32 +0200

    Update juggernaut.rb

--- yahki/recipes/juggernaut.rb
+++ yahki/recipes/juggernaut.rb
@@ -34,4 +34,29 @@ node[:deploy].each do |app_name, deploy|
     service juggernaut restart
     EOH
   end
+
+  template "juggernaut-rails" do
+    source "juggernaut-rails.erb"
+    path "/usr/local/bin/juggernaut-rails"
+  end
+
+  template "juggernaut-rails_init" do
+    source "juggernaut-rails.init.erb"
+    path "/etc/init.d/juggernaut-rails"
+  end
+
+  template "juggernaut-rails_conf" do
+    source "juggernaut-rails.conf.erb"
+    path "/etc/init/juggernaut-rails.conf"
+  end
+
+  script "juggernaut-rails_service" do
+    interpreter "bash"
+    user "root"
+    code <<-EOH
+    id -u juggernaut-rails &>/dev/null || adduser --system --no-create-home --disabled-login --disabled-password --group juggernaut-rails
+    chmod +x /etc/init.d/juggernaut-rails
+    update-rc.d -f juggernaut-rails defaults &>/dev/null
+    EOH
+  end
 end
