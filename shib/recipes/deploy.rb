a81da6d132305b40c3c0aa44727e86f5614db317
Ali Mohamed <ali.ahmmed.mohamed@gmail.com>
2015-02-10 12:53:20 +0200

    shib deploy fix

--- shib/recipes/deploy.rb
+++ shib/recipes/deploy.rb
@@ -1,65 +1,59 @@
-node[:deploy].each do |app_name, deploy|
-	
-	include_recipe 'deploy::default'
-  
-	script "deploy_clean" do
-		interpreter "bash"
-		user "root"
-		line = 10
-		sed_line = line - 2
-		vhost = "/etc/apache2/sites-available/#{app_name}.conf"
-		code <<-EOH
-		if ! sed -n '#{sed_line},$p' #{vhost} | grep "ShibUseHeaders On" ; then sed -i "#{line}i ShibUseHeaders On" #{vhost} ; fi ;
-		if ! sed -n '#{sed_line},$p' #{vhost} | grep "SetHandler shib" ; then sed -i "#{line}i SetHandler shib" #{vhost} ; fi ;
-		if ! sed -n '#{sed_line},$p' #{vhost} | grep "Require shibboleth" ; then sed -i "#{line}i Require shibboleth" #{vhost} ; fi ;
-		if ! sed -n '#{sed_line},$p' #{vhost} | grep "ShibRequireSession Off" ; then sed -i "#{line}i ShibRequireSession Off" #{vhost} ; fi ;
-		if ! sed -n '#{sed_line},$p' #{vhost} | grep "AuthType shibboleth" ; then sed -i "#{line}i AuthType shibboleth" #{vhost} ; fi ;
-		service apache2 restart
-		EOH
-	end  
-	
-	script "deploy_clean_https" do
-		if node[:deploy][app_name][:ssl_support]
-			interpreter "bash"
-			user "root"
-			line = 72
-			sed_line = line - 2
-			vhost = "/etc/apache2/sites-available/#{app_name}.conf"
-			code <<-EOH
-			if ! sed -n '#{sed_line},$p' #{vhost} | grep "ShibUseHeaders On" ; then sed -i "#{line}i ShibUseHeaders On" #{vhost} ; fi ;
-			if ! sed -n '#{sed_line},$p' #{vhost} | grep "SetHandler shib" ; then sed -i "#{line}i SetHandler shib" #{vhost} ; fi ;
-			if ! sed -n '#{sed_line},$p' #{vhost} | grep "Require shibboleth" ; then sed -i "#{line}i Require shibboleth" #{vhost} ; fi ;
-			if ! sed -n '#{sed_line},$p' #{vhost} | grep "ShibRequireSession Off" ; then sed -i "#{line}i ShibRequireSession Off" #{vhost} ; fi ;
-			if ! sed -n '#{sed_line},$p' #{vhost} | grep "AuthType shibboleth" ; then sed -i "#{line}i AuthType shibboleth" #{vhost} ; fi ;
-			service apache2 restart
-			EOH
-		end
-	end
+node[:deploy].each do |application, deploy_item|
 
-	template "shibboleth2" do
-		source "shibboleth2.xml.erb"
-		path "/etc/shibboleth/shibboleth2.xml"
-		variables(node['shib'])
-	end
-	
-	template "shibboleth2-ssl-key" do
-		mode 0644
-		source 'ssl.key.erb'
-		path "/etc/shibboleth/sp-key.pem"
-	end
-	
-	template "shibboleth2-ssl-cert" do
-		mode 0644
-		source 'ssl.cert.erb'
-		path "/etc/shibboleth/sp-cert.pem"
-	end
-	
-	script "deploy_shibd" do
-		interpreter "bash"
-		user "root"
-		code <<-EOH
-		sudo service apache2 restart
-		sudo service shibd restart
-	    	EOH
-	end
+  include_recipe 'deploy::default'
+
+  if deploy_item[:rails_env] == "production" && application == "yahki"
+	  script "deploy_apache_conf" do
+	    interpreter "bash"
+	    user "root"
+	    vhost_file = "/etc/apache2/sites-available/#{app_name}.conf"
+	    dir_tag = "<Directory #{deploy_item[:absolute_document_root]}>"
+	    shib_arr = ['AuthType shibboleth', 'ShibRequireSession Off', 'Require shibboleth',
+	      'SetHandler shib', 'ShibUseHeaders On']
+
+		  if File.exist?(vhost_file)      
+		    read_file = File.open(vhost_file, 'r').read
+		    out_file = File.open(vhost_file, 'w')
+		    prev_line = ''
+		    read_file.each_line do |line|
+		      if prev_line.include?(dir_tag) && ! line.include?(shib_arr[0])
+		        shib_arr.each do |shib_line|
+		          out_file.print "    #{shib_line}\n"
+		        end
+		        out_file.print "    #######\n"
+		      end
+		      out_file.print line
+		      prev_line = line
+		    end
+		    out_file.close
+		  end
+	  end
+  end
+
+  template "shibboleth2" do
+    source "shibboleth2.xml.erb"
+    path "/etc/shibboleth/shibboleth2.xml"
+    variables(node['shib'])
+  end
+
+  template "shibboleth2-ssl-key" do
+    mode 0644
+    source 'ssl.key.erb'
+    path "/etc/shibboleth/sp-key.pem"
+  end
+
+  template "shibboleth2-ssl-cert" do
+    mode 0644
+    source 'ssl.cert.erb'
+    path "/etc/shibboleth/sp-cert.pem"
+  end
+
+  script "deploy_shibd" do
+    interpreter "bash"
+    user "root"
+    code <<-EOH
+    sudo service apache2 restart
+    sudo service shibd restart
+    EOH
+  end
 end
