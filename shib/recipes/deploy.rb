171a40aae71f7fea46e53b3f6724c39a3c582c1c
Ali Mohamed <ali.ahmmed.mohamed@gmail.com>
2015-02-10 17:35:09 +0200

    temp fix

--- shib/recipes/deploy.rb
+++ shib/recipes/deploy.rb
@@ -3,40 +3,39 @@ node[:deploy].each do |application, deploy_item|
   include_recipe 'deploy::default'
 
   if deploy_item[:rails_env] == "production" && application == "yahki"
-	  template "vhost_file" do
-	    source "initial.erb"
-	    path "/etc/apache2/sites-available/#{application}.conf"
-	    variables({:content => deploy_apache_conf})
-	  end
-  end
+    template "vhost_file" do
+      source "initial.erb"
+      path "/etc/apache2/sites-available/#{application}.conf"
 
-  def deploy_apache_conf
-    vhost_file = "/etc/apache2/sites-available/#{application}.conf"
-    dir_tag = "<Directory #{deploy_item[:absolute_document_root]}>"
-    shib_arr = ['AuthType shibboleth', 'ShibRequireSession Off', 'Require shibboleth',
-      'SetHandler shib', 'ShibUseHeaders On']
-    Chef::Log.debug("document root: #{deploy_item[:absolute_document_root]}")
-    Chef::Log.debug("file: #{vhost_file}")
-    Chef::Log.debug("file exxists: #{File.exist?(vhost_file) ? 'yes' : 'no'}")
+      vhost_file = "/etc/apache2/sites-available/#{application}.conf"
+      dir_tag = "<Directory #{deploy_item[:absolute_document_root]}>"
+      shib_arr = ['AuthType shibboleth', 'ShibRequireSession Off', 'Require shibboleth',
+        'SetHandler shib', 'ShibUseHeaders On']
+      Chef::Log.debug("document root: #{deploy_item[:absolute_document_root]}")
+      Chef::Log.debug("file: #{vhost_file}")
+      Chef::Log.debug("file exxists: #{File.exist?(vhost_file) ? 'yes' : 'no'}")
 
-    if File.exist?(vhost_file)      
-      read_file = File.open(vhost_file, 'r').read
-      out_file = ''
-      prev_line = ''
-      read_file.each_line do |line|
-        if prev_line.include?(dir_tag) && ! line.include?(shib_arr[0])
-          shib_arr.each do |shib_line|
-            out_file += "    #{shib_line}\n"
+      if File.exist?(vhost_file)      
+        read_file = File.open(vhost_file, 'r').read
+        out_file = ''
+        prev_line = ''
+        read_file.each_line do |line|
+          if prev_line.include?(dir_tag) && ! line.include?(shib_arr[0])
+            shib_arr.each do |shib_line|
+              out_file += "    #{shib_line}\n"
+            end
+            out_file += "    #######\n"
           end
-          out_file += "    #######\n"
+          out_file += line
+          prev_line = line
         end
-        out_file += line
-        prev_line = line
       end
+      File.open(vhost_file, 'w') { |file| file.write(out_file) }
+      Chef::Log.debug("file content: #{out_file}")
+      out_file
+
+      variables({:content => out_file})
     end
-    File.open(vhost_file, 'w') { |file| file.write(out_file) }
-    Chef::Log.debug("file content: #{out_file}")
-    out_file
   end
 
   template "shibboleth2" do
