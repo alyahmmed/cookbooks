6623c1b0b7ea590422bdb0d8f49808531f2a57bd
Ali Mohamed <ali.ahmmed.mohamed@gmail.com>
2018-01-11 21:54:27 -0800

    removing librmagick-ruby package

--- yahki/recipes/image_magick.rb
+++ yahki/recipes/image_magick.rb
@@ -1,6 +1,6 @@
 node[:deploy].each do |app_name, deploy|
 
-  %w{imagemagick librmagick-ruby libmagickwand-dev}.each do |pkg|
+  %w{imagemagick ruby-rmagick libmagickcore-dev libmagickwand-dev}.each do |pkg|
 	package pkg do
 	end
   end
